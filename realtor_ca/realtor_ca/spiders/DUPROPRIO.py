e2c446b36e7654c9535e632ea687ccb297419e27
vezuras <74520628+vezuras@users.noreply.github.com>
2023-05-25 16:42:57 -0400

    Update DUPROPRIO.py

--- realtor_ca/realtor_ca/spiders/DUPROPRIO.py
+++ realtor_ca/realtor_ca/spiders/DUPROPRIO.py
@@ -7,7 +7,7 @@ class DUPROPRIOSpider(scrapy.Spider):
     name = 'DUPROPRIO'
     start_url = 'https://duproprio.com/fr/rechercher/liste?search=true&is_for_sale=1&with_builders=1&parent=1&pageNumber={}&sort=-published_at'
 
-    # custom_settings = {'CLOSESPIDER_PAGECOUNT': 24}
+    custom_settings = {'CLOSESPIDER_PAGECOUNT': 24}
 
     def start_requests(self):
         page_number = 1
@@ -24,21 +24,30 @@ class DUPROPRIOSpider(scrapy.Spider):
         yield scrapy.Request(url=next_page_url, meta={'page_number': next_page_number}, callback=self.parse)
 
     def parse_summary_page(self, response):
-        script_content = response.xpath("//script[contains(text(), 'SingleFamilyResidence')]/text()").get()
-        data = json.loads(script_content)
+        script_content_1 = response.xpath("//script[contains(text(), 'SingleFamilyResidence')]/text()").get()
+        data_1 = json.loads(script_content_1)
+        script_content_2 = response.xpath("//script[contains(text(), 'Product')]/text()").get()
+        data_2 = json.loads(script_content_2)
+
 
         title = response.xpath("//h3[@class='listing-location__title']/a/text()").get()
-        price = response.xpath("//div[@class='listing-price__amount']/text()").get()
+        # price = response.xpath("//div[@class='listing-price__amount']/text()").get()
         category = title.rsplit(' à vendre', 1)[0].strip()
-        price = price.strip()
-        street_address = data.get('address', {}).get('streetAddress', '')
-        locality = data.get('address', {}).get('addressLocality', '')
-        postal_code = data.get('address', {}).get('postalCode', '')
-        latitude = data.get('geo', {}).get('latitude', '')
-        longitude = data.get('geo', {}).get('longitude', '')
-        telephone = data.get('telephone', [])
+        # price = price.strip()
+        street_address = data_1.get('address', {}).get('streetAddress', '')
+        locality = data_1.get('address', {}).get('addressLocality', '')
+        postal_code = data_1.get('address', {}).get('postalCode', '')
+        latitude = data_1.get('geo', {}).get('latitude', '')
+        longitude = data_1.get('geo', {}).get('longitude', '')
+        telephone = data_1.get('telephone', [])
+        description = data_2.get('description', '')
+        images = data_2.get('image', [])
+        price = data_2.get('offers', {}).get('price', '')
+        url = data_2.get('offers', {}).get('url', '')
+        duproprio_id = data_2.get('sku', '')
 
         annonce = {
+            'source': 'duproprio',
             'category': category,
             'price': price,
             'street_address': street_address,
@@ -47,7 +56,10 @@ class DUPROPRIOSpider(scrapy.Spider):
             'latitude': latitude,
             'longitude': longitude,
             'telephone': telephone,
-            'url': response.url
+            'description': description,
+            'images': images,
+            'url': response.url,
+            'duproprio_id': duproprio_id
         }
 
         yield annonce
