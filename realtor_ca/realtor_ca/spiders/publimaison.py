3f6a8e1de1be53b643d9041e7715f18139473696
vezuras <74520628+vezuras@users.noreply.github.com>
2023-05-29 12:48:44 -0400

    Update publimaison.py

--- realtor_ca/realtor_ca/spiders/publimaison.py
+++ realtor_ca/realtor_ca/spiders/publimaison.py
@@ -32,13 +32,36 @@ class PublimaisonSpider(scrapy.Spider):
             )
 
     def parse_summary(self, response):
+        cookie_request_verification_token = response.headers.getlist('Set-Cookie')
+        cookie_request_verification_token = cookie_request_verification_token[0].decode('utf-8') if cookie_request_verification_token else ''
+        cookie_publimaisonalertelang = response.headers.getlist('Set-Cookie')
+        cookie_publimaisonalertelang = cookie_publimaisonalertelang[1].decode('utf-8') if len(cookie_publimaisonalertelang) > 1 else ''
+        request_verification_token = response.xpath('//input[@name="__RequestVerificationToken"]/@value').get()
+        hash_value = response.xpath("//span[@class='telephone']/@data-url").get()
         category = response.xpath("(//div[@class='one columns'])[1]/ul/li[2]/div/text()").get()
         price = response.xpath("//div[@class='prix']/h3/text()").get()
         map_url = response.url + "/carte"
+        # Construire les données pour la requête AJAX
+        data = {
+            '__RequestVerificationToken': request_verification_token,
+            'hash': hash_value,
+        }
 
-        # Extraire le jeton de vérification de la requête '__RequestVerificationToken'
-        request_verification_token = response.css('input[name="__RequestVerificationToken"]::attr(value)').get()
-        
+        # Envoyer la requête AJAX avec les cookies inclus dans les headers
+        ajax_url = 'https://www.publimaison.ca/StatCounter/Telephone'
+        headers = {
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',  # Ajouter l'en-tête Content-Type
+            'Cookie': f'{cookie_request_verification_token}; {cookie_publimaisonalertelang}'  # Inclure les cookies dans les headers de la requête
+        }
+        yield scrapy.FormRequest(
+            url=ajax_url,
+            method='POST',
+            formdata=data,
+            headers=headers,
+            callback=self.parse_telephones
+        )
+
         annonce = {
             'category': category,
             'price': price,
@@ -94,15 +117,11 @@ class PublimaisonSpider(scrapy.Spider):
             else:
                 self.logger.info("Impossible d'extraire la latitude et la longitude")
 
-    def parse_telephone_response(self, response):
-        annonce = response.meta['annonce']
-        telephone_data = json.loads(response.text)
-
-        telephone = telephone_data['value']
-        annonce['telephone'] = telephone
-
-        yield annonce
-
-    def decode_telephone(self, url):
-        url = urllib.parse.unquote(url)
-        return url.replace("%C2%A0", "").replace("%2b", "+").replace("%2f", "/")
+    def parse_telephones(self, response):
+        data = json.loads(response.text)
+        phone_number = data.get("value")
+        
+        if phone_number:
+            print("Phone number:", phone_number)
+        else:
+            print("No phone number found")
