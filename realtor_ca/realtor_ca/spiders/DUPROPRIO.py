a989f9a358fa66df09e899e623ca13a52365668a
vezuras <74520628+vezuras@users.noreply.github.com>
2023-06-13 14:47:01 -0400

    Update DUPROPRIO.py

--- realtor_ca/realtor_ca/spiders/DUPROPRIO.py
+++ realtor_ca/realtor_ca/spiders/DUPROPRIO.py
@@ -4,10 +4,10 @@ import csv
 from datetime import datetime
 
 class DUPROPRIOSpider(scrapy.Spider):
-    name = 'DUPROPRIO'
+    name = 'duproprio'
     start_url = 'https://duproprio.com/fr/rechercher/liste?search=true&is_for_sale=1&with_builders=1&parent=1&pageNumber={}&sort=-published_at'
 
-    custom_settings = {'CLOSESPIDER_PAGECOUNT': 24}
+    # custom_settings = {'CLOSESPIDER_PAGECOUNT': 10}
 
     def start_requests(self):
         page_number = 1
@@ -16,14 +16,16 @@ class DUPROPRIOSpider(scrapy.Spider):
     def parse(self, response):
         page_number = response.meta['page_number']
         annonce_urls = response.xpath("//ul[@class='search-results-listings-list']/li/a/@href").getall()
+        category = response.xpath("//div[@class='box-result-unit-type']/p/text()").get()
         for url in annonce_urls:
-            yield scrapy.Request(url=url, callback=self.parse_summary_page)
+            yield scrapy.Request(url=url, callback=self.parse_summary_page, meta={'category': category})
 
         next_page_number = page_number + 1
         next_page_url = self.start_url.format(next_page_number)
         yield scrapy.Request(url=next_page_url, meta={'page_number': next_page_number}, callback=self.parse)
 
     def parse_summary_page(self, response):
+        
         script_content_1 = response.xpath("//script[contains(text(), 'SingleFamilyResidence')]/text()").get()
         data_1 = json.loads(script_content_1)
         script_content_2 = response.xpath("//script[contains(text(), 'Product')]/text()").get()
@@ -31,35 +33,43 @@ class DUPROPRIOSpider(scrapy.Spider):
 
 
         title = response.xpath("//h3[@class='listing-location__title']/a/text()").get()
-        # price = response.xpath("//div[@class='listing-price__amount']/text()").get()
         category = title.rsplit(' à vendre', 1)[0].strip()
-        # price = price.strip()
-        street_address = data_1.get('address', {}).get('streetAddress', '')
-        locality = data_1.get('address', {}).get('addressLocality', '')
-        postal_code = data_1.get('address', {}).get('postalCode', '')
-        latitude = data_1.get('geo', {}).get('latitude', '')
-        longitude = data_1.get('geo', {}).get('longitude', '')
-        telephone = data_1.get('telephone', [])
-        description = data_2.get('description', '')
-        images = data_2.get('image', [])
-        price = data_2.get('offers', {}).get('price', '')
-        url = data_2.get('offers', {}).get('url', '')
-        duproprio_id = data_2.get('sku', '')
+        number_of_rooms = data_1.get('numberOfRooms')
+        address = data_1.get('address', {})
+        street_address = address.get('streetAddress')
+        locality = address.get('addressLocality')
+        postal_code = address.get('postalCode')
+        geo = data_1.get('geo', {})
+        latitude = geo.get('latitude')
+        longitude = geo.get('longitude')
+        telephone = data_1.get('telephone')
+        url = data_1.get('url')
+
+        description = data_2.get('description')
+        image = data_2.get('image')
+        offers = data_2.get('offers', {})
+        price = offers.get('price')
+        price_currency = offers.get('priceCurrency')
+        url = offers.get('url')
+        sku = data_2.get('sku')
 
         annonce = {
-            'source': 'duproprio',
+            'date': datetime.now().strftime("%Y-%m-%d"),
+            'source': self.name,
             'category': category,
-            'price': price,
-            'street_address': street_address,
-            'locality': locality,
-            'postal_code': postal_code,
+            'prix': price,
+            'address': {
+                'street_address': street_address,
+                'locality': locality,
+                'region': "",
+                'postal_code': postal_code,
+            },
+            'telephone': telephone,
             'latitude': latitude,
             'longitude': longitude,
-            'telephone': telephone,
             'description': description,
-            'images': images,
-            'url': response.url,
-            'duproprio_id': duproprio_id
+            'url': url,
+            'sku': f"{self.name}-{sku}"
         }
 
         yield annonce
